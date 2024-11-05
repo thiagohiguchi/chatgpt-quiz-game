32b5a2d96811e3706836080612ade94b16cacd36
Thiago Higuchi <thiago.higuchi@gmail.com>
2024-11-05 02:44:20 -0300

    Fixed linter

--- eslint.config.js
+++ eslint.config.js
@@ -1,31 +1,33 @@
-import js from '@eslint/js';
 import reactHooks from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
 import typescriptEslintParser from '@typescript-eslint/parser';
+import eslintPluginPrettier from 'eslint-plugin-prettier';
+import reactRefresh from 'eslint-plugin-react-refresh';
 import eslintPluginImport from 'eslint-plugin-import';
 import eslintPluginSimpleImportSort from 'eslint-plugin-simple-import-sort';
-import eslintPluginPrettier from 'eslint-plugin-prettier';
 import typescriptEslintPlugin from '@typescript-eslint/eslint-plugin';
+import js from '@eslint/js';
 
-// Define the ESLint configuration
+
 export default [
   {
     ignores: ['dist', 'node_modules'],
     languageOptions: {
       parser: typescriptEslintParser, // Use TypeScript parser
     },
-    files: ['**/*.{ts,tsx}'],
+    files: ['**/*.{ts,tsx,js}'],
+
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
-      '@typescript-eslint': typescriptEslintPlugin, // TypeScript plugin
+      '@typescript-eslint': typescriptEslintPlugin, 
       import: eslintPluginImport,
       'simple-import-sort': eslintPluginSimpleImportSort,
-      prettier: eslintPluginPrettier, // Add Prettier plugin
+      prettier: eslintPluginPrettier, 
     },
-    rules: {
-      // Add rules from the recommended configurations
-      ...js.configs.recommended.rules,
+      rules: {
+          // semi: "error",
+          // "prefer-const": "error"
+          ...js.configs.recommended.rules,
       ...typescriptEslintPlugin.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
 
@@ -33,9 +35,12 @@ export default [
         'warn',
         { allowConstantExport: true },
       ],
+
       // Import sorting rules
       'simple-import-sort/imports': 'error',
       'simple-import-sort/exports': 'error',
+
+   
       // Optional: Enforce grouped ordering for better readability
       'import/order': [
         'error',
@@ -49,9 +54,12 @@ export default [
           'newlines-between': 'always',
         },
       ],
+
       // Optional: Additional rules
       'no-duplicate-imports': 'error',
-      'import/no-unused-modules': [1, { unusedExports: true }],
-    },
-  },
+      // 'import/no-unused-modules': [1, { unusedExports: true }],
+      // 'import/no-unused-modules': [1, { unusedExports: true }], // Warn on unused exports
+
+      }
+  }
 ];
